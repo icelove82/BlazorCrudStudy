a3b6efc44e8fd8964e3e79a42181097ff1773adb
YUN <yunmyeonghun@gmail.com>
2022-03-07 22:32:29 +0900

    study 04 Add temp data logic

--- BlazorCrudStudy/Server/Controllers/SuperHeroController.cs
+++ BlazorCrudStudy/Server/Controllers/SuperHeroController.cs
@@ -8,10 +8,36 @@ namespace BlazorCrudStudy.Server.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        public static List<Comic> comics = new List<Comic>
+        {
+            new Comic{ Id = 1, Name = "Marvel"},
+            new Comic{ Id = 2, Name ="DC"}
+        };
+
+        public static List<SuperHero> heros = new List<SuperHero>
+        {
+            new SuperHero
+            {
+                Id = 1,
+                FirstName = "Peter",
+                LastName = "Parker",
+                HeroName = "Spiderman",
+                Comic = comics[0]
+            },
+            new SuperHero
+            {
+                Id = 2,
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                HeroName = "Batman",
+                Comic = comics[1]
+            }
+        };
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetSuperHeros()
         {
-            return Ok();
+            return Ok(heros);
         }
 
         /*[HttpGet]
@@ -19,7 +45,12 @@ namespace BlazorCrudStudy.Server.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
         {
-            return Ok();
+            var hero = heros.FirstOrDefault(hero => hero.Id == id);
+            if (hero == null)
+            {
+                return NotFound("Sorry, no hero here. :/");
+            }
+            return Ok(hero);
         }
     }
 }
